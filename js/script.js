2f481ae788cd902f74ef8a8216e4994b7f180ec8
YannHyaric <ylhalmeida@gmail.com>
2020-11-18 19:54:35 -0300

    Updates

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   if (document.getElementsByTagName("html")[0].getAttribute("lang") == "br") {
     var typed = new Typed(".typing", {
       strings: ["Desenvolvedor", "Programador", "Engenheiro de Software"],
-      typeSpeed: 100,
-      backSpeed: 60,
+      typeSpeed: 80,
+      backSpeed: 40,
       loop: true,
     });
   } else if (
@@ -12,15 +12,15 @@ $(document).ready(function () {
   ) {
     var typed = new Typed(".typing", {
       strings: ["Développeur", "Programmeur", "Ingénieur logiciel"],
-      typeSpeed: 100,
-      backSpeed: 60,
+      typeSpeed: 80,
+      backSpeed: 40,
       loop: true,
     });
   } else {
     var typed = new Typed(".typing", {
       strings: ["Developer", "Programmer", "Software Engineer"],
-      typeSpeed: 100,
-      backSpeed: 60,
+      typeSpeed: 80,
+      backSpeed: 40,
       loop: true,
     });
   }
